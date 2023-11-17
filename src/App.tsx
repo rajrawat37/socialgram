9f2edac9a70c3cb4d2dbe4d611ff18a1f9504507
rajrawat37 <rawatraj995@gmail.com>
2023-11-17 11:58:52 +0530

    tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './globals.css';
+const App = () => {
+  return (
+    <>
+      <div className=" font-bold underline text-3xl w-1/2 text-center"> App</div>
+   </>
+  )
+}
+export default App;
